7f75d5512aa75efd8b97c5b08789db2963247ff6
Aza-Palitex-777 <163240555+Aza-Palitex-777@users.noreply.github.com>
2024-03-13 11:48:11 +0500

    I don't love writing shitty code

--- js/sedndForms.js
+++ js/sedndForms.js
@@ -1,32 +1,21 @@
 const form = document. querySelector('.form-test-drive')
-//https://jsonplaceholder.typicode.com
+const api = "https://jsonplaceholder.typicode.com";
 form.addEventListener ('submit', (event) => {
     event.preventDefault()
-
     let data = {}
+    for (let { name, value } of form.elements) if (name)data[name] = value
 
-    for (let { name, value } of form.elements) {
-        if (name) {
-            data[name] = value
-        }
-    }
-
-    fetch('https://jsonplaceholder.typicode.com/posts', {
+    fetch(api+ '/posts', {
         method: 'POST',
         body: JSON.stringify(data)
-    })
-        .then(response => {
-            if (response.status === 200 || response.status === 201) { }
-            return response.json()
+    }).then(response => {
+            if (response.status === 200 || response.status === 201) return response.json();
         } else{
-            throw new Error(response.status)
+            throw new Error(response.status);
         })
     then(data => {
-        alert ('Данные успешно сохранены')
-        form.reset()
-    })
-        .catch(error => {
-            alert('Произошла ошибка, статус' + error.message)
-    })  
+        alert ('Данные успешно сохранены');
+        form.reset();
+    }).catch(error => alert('Произошла ошибка, статус' + error.message))  
 })
 
